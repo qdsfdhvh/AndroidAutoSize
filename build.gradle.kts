060359cefbb7631e6bbefc4ce75dd46fee69345b
seiko <605590140@qq.com>
2020-07-31 17:31:33 +0800

    Upload Bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ buildscript {
     dependencies {
         classpath(Deps.gradle)
         classpath(kotlin(module = "gradle-plugin", version = Versions.kotlin))
+
+        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.18")
     }
 }
 
